e924c5f131da971c1ac120112d6c2f0bd30239c8
Karthickravi138 <141944186+Karthickravi138@users.noreply.github.com>
2024-10-18 13:15:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -124,6 +124,7 @@ def predict():
         return render_template("result.html", prediction_text="Your Predicted Life Expectancy is: {}".format(prediction), data_from_db=data_from_db)
 
     return render_template("predict.html")
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
+
